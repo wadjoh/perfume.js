c2e851cc2dd0296048ef0ae702989e91a6880ebd
Wade Johnson <wadej93@gmail.com>
2023-12-13 18:57:13 -0800

    use swc and typescript rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,76 +1,102 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import { RollupOptions, ModuleFormat } from 'rollup';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import swc from '@rollup/plugin-swc';
+import pkg from './package.json' assert { type: 'json' };
 
-const createConfig = ({ output, min = false }) => {
-  const minify =
-    min &&
-    terser({
-      output: {
-        comments(_, { text, type }) {
-          if (type === 'comment2') {
-            // multiline comment
-            return /@preserve|@license|@cc_on/i.test(text);
-          }
+enum FormatEnum {
+  ESM = 'esm',
+  CJS = 'cjs',
+  IIFE = 'iife',
+  UMD = 'umd',
+}
+
+const createConfig = ({
+  output,
+  min = false,
+}: {
+  output: { file: string; format: ModuleFormat };
+  min?: boolean;
+}) => {
+  const swcPlugin = swc({
+    swc: {
+      minify: min,
+      jsc: {
+        parser: {
+          syntax: 'typescript',
+          tsx: false,
+          decorators: true,
+        },
+        transform: {
+          legacyDecorator: true,
+          decoratorMetadata: true,
+        },
+        target: 'es5',
+        minify: {
+          sourceMap: true,
+          compress: {
+            defaults: true,
+            pure_getters: 'strict',
+            reduce_funcs: true,
+            top_retain: null,
+          },
+          mangle: true,
         },
       },
-    });
+      module: {
+        type: output.format === FormatEnum.ESM ? 'es6' : 'commonjs',
+        strict: true,
+      },
+    },
+  });
 
   const plugins = [
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-    // Resolve source maps to the original source
-    sourceMaps(),
-    minify,
+    nodeResolve({ extensions: ['.ts'] }),
+    swcPlugin,
   ];
 
-  if (output.format !== 'esm') {
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    plugins.unshift(commonjs());
-  }
-
   return {
-    input: 'dist/es/perfume.js',
+    input: 'src/perfume.ts',
     output: {
       file: output.file,
       format: output.format,
       name: 'Perfume',
-      sourcemap: true
+      sourcemap: true,
     },
-    watch: { include: 'dist/es/**' },
-    plugins: plugins.filter(Boolean),
+    watch: { include: 'dist/**' },
+    plugins,
   };
 };
 
-export default [
+const bundle: RollupOptions[] = [
   createConfig({
-    output: { file: pkg.module, format: 'esm' }
+    output: { file: pkg.module, format: FormatEnum.ESM },
   }),
   createConfig({
-    output: { file: pkg.main, format: 'cjs' }
+    output: { file: pkg.main, format: FormatEnum.CJS },
   }),
   createConfig({
-    output: { file: 'dist/perfume.esm.min.js', format: 'esm' },
+    output: { file: 'dist/perfume.esm.min.js', format: FormatEnum.ESM },
     min: true,
   }),
   createConfig({
-    output: { file: 'dist/perfume.min.js', format: 'cjs' },
+    output: { file: 'dist/perfume.min.js', format: FormatEnum.CJS },
     min: true,
   }),
   createConfig({
-    output: { file: pkg.iife, format: 'iife' },
+    output: { file: pkg.iife, format: FormatEnum.IIFE },
   }),
   createConfig({
-    output: { file: 'dist/perfume.iife.min.js', format: 'iife' },
+    output: { file: 'dist/perfume.iife.min.js', format: FormatEnum.IIFE },
     min: true,
   }),
   createConfig({
-    output: { file: pkg.unpkg, format: 'umd' },
+    output: { file: pkg.unpkg, format: FormatEnum.UMD },
   }),
   createConfig({
-    output: { file: 'dist/perfume.umd.min.js', format: 'umd' },
+    output: { file: 'dist/perfume.umd.min.js', format: FormatEnum.UMD },
     min: true,
   }),
 ];
+
+export default bundle;
